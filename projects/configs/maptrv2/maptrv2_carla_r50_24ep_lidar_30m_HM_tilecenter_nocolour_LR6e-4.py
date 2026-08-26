a426d73487c732f84051e19fe670bc1365b92602
JackHilliard <jackhilliard90@gmail.com>
2026-08-25 20:39:01 -0400

    added new config for EMD loss that uses a LR of 6e-4 to match the vanilla maptrv2 run.

---
+++ projects/configs/maptrv2/maptrv2_carla_r50_24ep_lidar_30m_HM_tilecenter_nocolour_LR6e-4.py
@@ -0,0 +1,8 @@
+"""Colour-free variant of the 30m tile-centred HM (polyline geometry) config with lr=6e-4.
+
+"""
+
+_base_ = ['./maptrv2_carla_r50_24ep_lidar_30m_HM_tilecenter_nocolour.py']
+
+optimizer = dict(type='AdamW', lr=6e-4, weight_decay=0.01)
+
